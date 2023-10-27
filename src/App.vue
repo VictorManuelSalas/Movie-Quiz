c14b348172705105a6b3af81710ce5cce0dcb41c
Víctor Manuel Nuñez Sandra <98570683+VictorManuelSalas@users.noreply.github.com>
2023-10-27 10:09:44 -0600

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <main v-if="changeView" id="default">
+    <main v-if="!changeView" id="default">
       <img src="./assets/question.png" alt="" />
       <button @click="changeView = false">Start Game</button>
     </main>
-    <main v-if="!changeView">
-      <h1>Sound #1</h1>
+    <main v-if="changeView">
+      <h1>Quiz Sounds</h1>
       <div id="container">
         <strong>Question</strong>
         <p>Who is this person?</p>
@@ -32,10 +32,513 @@
                   </audio>
                 </li>
               </ul>
-              <input type="checkbox" name="" id="check" @change="checkOne = !checkOne">
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
             </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/bob y patrick.jpg" alt="draick" />
+              <ul>
+                <li>Sound #2</li>
+                <li>Answer: Bob esponja</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/bob esponja.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download
+                      <a href="./assets/music/bob esponja.mp4">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/tortugasdenemo.jpg" alt="draick" />
+              <ul>
+                <li>Sound #3</li>
+                <li>Answer: Tortugas de nemo (Squirt)</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/buscando a nemo-tortugas.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download
+                      <a href="./assets/music/buscando a nemo-tortugas.mp4"
+                        >MP3</a
+                      >
+                      or <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/donramon.jpg" alt="draick" />
+              <ul>
+                <li>Sound #4</li>
+                <li>Answer: Don Ramon</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/chavo del 8-ramon.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download
+                      <a href="./assets/music/chavo del 8-ramon.mp4">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/donde estan las rubias.jpg" alt="draick" />
+              <ul>
+                <li>Sound #5</li>
+                <li>Answer: Marcus y Kevin (Donde estan las rubias)</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/donde estan las rubias.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/crud y capitan.jpg" alt="draick" />
+              <ul>
+                <li>Sound #6</li>
+                <li>Answer: Grud y Capitan America (Avengers Endgame)</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/grud y capitanamerica.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/gump.jpg" alt="draick" />
+              <ul>
+                <li>Sound #7</li>
+                <li>Answer: Forrest Gump</li>
+                <li>
+                  <audio controls>
+                    <source src="./assets/music/gump.mp4" type="audio/mpeg" />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/ron y hermony.jpg" alt="draick" />
+              <ul>
+                <li>Sound #8</li>
+                <li>Answer: Ron y Hermonie</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/harry potter ron y hermony.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/dombuldor.jpg" alt="draick" />
+              <ul>
+                <li>Sound #9</li>
+                <li>Answer: Dombuldor</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/HarryPotterDombuldor.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/jenji y lord.jpeg" alt="draick" />
+              <ul>
+                <li>Sound #10</li>
+                <li>Answer: Jenjibre y LordFargat (Sherk)</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/jenjibre sherk y lord farguat.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/homero y miljaus.jpg" alt="draick" />
+              <ul>
+                <li>Sound #11</li>
+                <li>Answer: Homero y Miljaus</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/los simposon-homero y miljaus.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/tony stark.png" alt="draick" />
+              <ul>
+                <li>Sound #12</li>
+                <li>Answer: Tony Stark (Avengers)</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/Los Vengadores-tony.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/lord fargaut.jpg" alt="draick" />
+              <ul>
+                <li>Sound #13</li>
+                <li>Answer: Lord Farguat</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/lord farguat sherk.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/skiper magadascar.jpg" alt="draick" />
+              <ul>
+                <li>Sound #14</li>
+                <li>Answer: Skiper (Madagascar)</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/madagascar-skiper.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/mario bross estrella.jpg" alt="draick" />
+              <ul>
+                <li>Sound #15</li>
+                <li>Answer: Lumalee (Mario Bross)</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/mariobross- estrella azul.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/monsterinc 1.jpeg" alt="draick" />
+              <ul>
+                <li>Sound #16</li>
+                <li>Answer: Monster Inc</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/monser inc 1.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/burro.jpeg" alt="draick" />
+              <ul>
+                <li>Sound #17</li>
+                <li>Answer: Burro (Sherk)</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/sherk burro.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/locutor spider man.jpeg" alt="draick" />
+              <ul>
+                <li>Sound #18</li>
+                <li>Answer: Spiderman</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/spider-man locutor.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img src="./assets/img/thore.jpg" alt="draick" />
+              <ul>
+                <li>Sound #19</li>
+                <li>Answer: Thor</li>
+                <li>
+                  <audio controls>
+                    <source src="./assets/music/thor.mp4" type="audio/mpeg" />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+            <!-- Aqui empieza otro-->
+            <div v-if="checkOne">
+              <img
+                src="./assets/img/monster inc pejelagarto.jpg"
+                alt="draick"
+              />
+              <ul>
+                <li>Sound #20</li>
+                <li>Answer: Monster Inc.</li>
+                <li>
+                  <audio controls>
+                    <source
+                      src="./assets/music/monter inc.mp4"
+                      type="audio/mpeg"
+                    />
+                    <p>
+                      Download <a href="myAudio.mp3">MP3</a> or
+                      <a href="myAudio.ogg">OGG</a> audio.
+                    </p>
+                  </audio>
+                </li>
+              </ul>
+              <input
+                type="checkbox"
+                name=""
+                id="check"
+                @change="checkOne = !checkOne"
+              />
+            </div>
+
+            <!-- Aqui empieza otro-->
           </div>
-        <!-- 
+          <!-- 
 
      
           <span>
@@ -72,6 +575,7 @@ export default {
   components: {},
   data() {
     return {
+      t: "./assets/img/drackandjosh.png",
       test: false,
       checkOne: true,
       changeView: true,
@@ -88,11 +592,9 @@ export default {
 </script>
 
 <style scoped>
-#check{
-  
+#check {
 }
 
- 
 #default {
   width: 100%;
   height: 100vh;
@@ -179,7 +681,7 @@ section {
 }
 #elements div img {
   width: 150px;
-  height:auto;
+  height: auto;
   border-radius: 1rem;
   margin: 0%;
 }
